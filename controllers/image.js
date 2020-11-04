75950a557197313d214dcf8c39a92f40a74073ba
C Tompkins <celloviolinbass@yahoo.com>
2020-11-04 12:36:40 -0600

    initial commit

---
+++ controllers/image.js
@@ -0,0 +1,32 @@
+const { json } = require("body-parser");
+const Clarifai = require("clarifai");
+
+const app = new Clarifai.App({
+  apiKey: "c197c65899924fd5a1892b8d5db0a364",
+});
+
+const handleApiCall = (req, res) => {
+  app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => {
+        res.json(data);
+    })
+    .catch(err => res.status(400).json("unable to work with API"))
+};
+
+const handleImage = (req, res, db) => {
+  const { id } = req.body;
+  db("users")
+    .where("id", "=", id)
+    .increment("entries", 1)
+    .returning("entries")
+    .then((entries) => {
+      res.json(entries[0]);
+    })
+    .catch((err) => res.status(400).json("unable to get entries"));
+};
+
+module.exports = {
+  handleImage,
+  handleApiCall
+};
