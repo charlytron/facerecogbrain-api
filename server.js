fc64bad1c0bd5cfb69ab4c8665b07ffc0d194c96
C Tompkins <celloviolinbass@yahoo.com>
2020-11-06 09:54:08 -0600

    git commit -m "app.listen change line 61"

--- server.js
+++ server.js
@@ -47,13 +47,17 @@ app.get("/profile/:id", (req, res) => {
   profile.handleProfileGet(req, res, db);
 });
 app.put("/image", (req, res) => {
-image.handleImage(req, res, db);
+  image.handleImage(req, res, db);
 });
 
 app.post("/imageurl", (req, res) => {
   image.handleApiCall(req, res);
 });
 
+// app.listen(process.env.port || 3000, () => {
+//   console.log(`app is running on ${process.env.PORT}`);
+// });
+
 app.listen(3000, () => {
-  console.log("app is running on port 3001");
+  console.log("app is running on port 3000");
 });
